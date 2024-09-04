58b473d150423917d4590b875f5ecf69e1f874c2
emiliasmoothbrain <emiliasaez@lcad.edu>
2024-09-04 15:18:15 -0700

    attempted to write code for the shrimp

--- Assets/scripts/emiliaSaezReferenceAndFunctionPractice.cs
+++ Assets/scripts/emiliaSaezReferenceAndFunctionPractice.cs
@@ -2,15 +2,27 @@ using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class emiliasaezReferenceAndFunctionPractice : MonoBehaviour
 {
-    public Image shrimpSprite;
-    public Image fireSprite;
-    
+    public Image fireref;
+    public Image ingredientRef;
+    public Image cookedFoodRef;
+
+
+    public Sprite FireForPan;
+    public Sprite rawIngredients;
+    public Sprite cookedRice;
     // Start is called before the first frame update
     void Start()
     {
+        fireref.Sprite = FireForPan;
+
+        FireForPan.Sprite.SetActive(false);
+      
+      //GameObject.Find("ingredientsAndFire").SetActive(false);
+
         
     }
 
